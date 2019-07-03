6ca6cdd1c6880d450b035bab4217f0fca4a27f07
Tina Le <tinale09@gmail.com>
2019-07-03 16:36:32 -0700

    complete server and webpack setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
+const express = require ('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`))
+
+app.use(express.static('/Users/TinaLe/gallery/client/dist'))
+
+
 
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    entry: __dirname + '/client/src/index.jsx',
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react', '@babel/preset-env']
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/client/dist'
+    }
+}; 
