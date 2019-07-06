c720993c468cf7dc3bd5ced694f53d97723d1ded
Tina Le <tinale09@gmail.com>
2019-07-06 10:52:47 -0700

    complete client request and server response for initial get

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require ('express');
 const app = express();
 const PORT = 3000;
+const db = require ('./db/index.js');
 
 
 app.use(express.static('/Users/TinaLe/gallery/client/dist'))
 
 
 app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`))
+
+app.get('/gallery', (req, res)=>{
+    db.query("SELECT * from users, images where images.userID = users.id", (err, data)=>{
+        if (err) {
+            res.status(500).send(err); 
+        } else {
+            res.send(data); 
+        }
+    })
+})
 
