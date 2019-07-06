8091d046c10b6b542dfff08cb5d09a804a6bc4ad
Tina Le <tinale09@gmail.com>
2019-07-05 20:45:03 -0700

    complete database seeding with bulk insert

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,69 +1,81 @@
 var faker = require('faker');
-var db = require('./index.js')
+var db = require('./index.js');
 
-//SEED USERS
-
-//users without images
+//Function to generate Users without Images Array
 
 const seedUsers = () => {
+  let users = [];
   let count = 0; 
   while (count < 20){
-    db.query('insert into users (name) values (?)', [faker.name.findName()], (error) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('success');
-      }
-    })
+    users.push([faker.name.findName()]);  
     count++; 
   }
-}
+  return users; 
+}; 
 
-//user with images
+//Function to generate Users with Images Array 
 
-const seedURLUser = (userURL) => {
-  db.query('insert into users (name, URL) values (?, ?)', [faker.name.findName(), userURL], (error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('success');
-  }
-})
-};
-
-//all users (with and without URL images)
-const seedAllUsers = () => {
+const seedURLUsers = () => {
+  let urlUsers = []; 
   for (let i = 1; i < 21; i++){
-    seedURLUser(`https://munch-gallery.s3-us-west-1.amazonaws.com/Users/users${i}.jpg`); 
+    urlUsers.push([faker.name.findName(), `https://munch-gallery.s3-us-west-1.amazonaws.com/Users/users${i}.jpg`]); 
   };
-  seedUsers(); 
-}; 
-
-//Seed Images
-
-const seedImage = (imageURL) => {
-   db.query('insert into images (URL, caption, userID) values (?, ?, ?)', [imageURL, faker.lorem.words(), Math.ceil(Math.random() * 40)], (error) => {
-  if (error) {
-      console.log(error);
-  } else {
-    console.log('success');
-  }
-}
-)};
-
-const seedAllImages = () => {
-  for (let i=1; i < 101; i++){
-    seedImage(`https://munch-gallery.s3-us-west-1.amazonaws.com/munch${i}.jpg`)
-  }
-}; 
-
-//Call all functions to seed database
-
-const seedDatabase = () => {
-  seedAllUsers(); 
-  seedAllImages(); 
+  return urlUsers; 
 };
 
-//Invoking function to seed database
+//Function to generate Images Information Array 
 
-seedDatabase(); 
+const seedImages = () => {
+  let images = [];
+  for (let i = 1; i < 101; i++){
+    images.push([`https://munch-gallery.s3-us-west-1.amazonaws.com/munch${i}.jpg`, faker.lorem.words(), Math.ceil(Math.random() * 40)]);
+  }
+  return images; 
+}; 
+
+//Function to populate database with users w/o images
+
+const seedAllUsers = (callback) => {
+  let users = seedUsers();
+  let query = 'INSERT INTO users (name) VALUES ?';
+  db.query (query, [users], (err)=>{
+    if (err){
+      console.log(err);
+    } else {
+      callback(); 
+    }
+  })
+}; 
+
+//Function to populate database with users w/ images
+
+const seedAllURLUsers = (callback) => {
+  let urlUsers = seedURLUsers();
+  db.query ('insert into users (name, URL) values ?', [urlUsers], (err)=>{
+    if (err){
+      console.log(err);
+    } else {
+      callback(); 
+    }
+  })
+}; 
+
+//Function to populate database with images information 
+
+const seedAllImages = (callback) => {
+  let images = seedImages(); 
+  db.query('insert into images (URL, caption, userID) values ?', [images], (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        callback(null, 'Seeding successful');
+      }
+    })
+};
+
+//Seed Database with all functions
+
+seedAllUsers(()=>seedAllURLUsers(()=>seedAllImages((err, message)=>console.log(message))));
+
+
+
