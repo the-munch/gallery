b8488459ca7ed8656d5ec6b1d2052e55c8707922
Tina Le <tinale09@gmail.com>
2019-07-08 17:54:32 -0700

    complete format captions with user images

--- client/src/components/CenterImage.jsx
+++ client/src/components/CenterImage.jsx
@@ -3,14 +3,21 @@ import styles from './style/CenterImage.css';
 
 const CenterImage = (props) => {
     let style;
+    let capt;
     if(props.hover) {
-        style = styles.centerFoodImgSmall
+        style = styles.centerFoodImgSmall;
+        capt = styles.none; 
     } else {
-        style = styles.centerFoodImg
+        style = styles.centerFoodImg;
+        capt = styles.caption;
     }
     return (
-        <a href='' onClick ={(e)=>props.onOpenClick(e, props.image.URL)}>
+        <a href='' onClick ={(e)=>props.onOpenClick(e, props.image.URL)} className={styles.overlay}>
             <img src={props.image.URL} className={style} style={{objectFit: 'cover'}}/>
+            <div className={capt}>
+                {props.image.userURL && <img src={props.image.userURL} className={styles.userImage} />}
+                <p> <span className={styles.description}>{props.image.caption}</span> by {props.image.name}</p>
+            </div>
         </a>
     )
 };
--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import styles from './style/Image.css';
 
-const Image = (props) => (
-        <a href='' onClick ={(e)=>props.onOpenClick(e, props.image.URL)} className={styles.overlay}> 
-            <img 
+const Image = (props) => {
+    return (
+    <a href='' onClick ={(e)=>props.onOpenClick(e, props.image.URL)} className={styles.overlay}> 
+        <img 
             onMouseOver={()=>props.onHover()}
             onMouseOut={()=>props.onHoverOut()}
             src={props.image.URL} 
             className={styles.foodImg} 
             style={{objectFit: 'cover'}}
-            />
-            <div class={styles.after}>{props.image.caption}</div>
-        </a> 
-)
+        />
+        <div className={styles.caption}>
+            {props.image.userURL && <img src={props.image.userURL} className={styles.userImage} />}
+            <p> <span className={styles.description}>{props.image.caption}</span> by {props.image.name}</p>
+        </div>
+    </a> 
+    )
+}
+
+
+
 
 
 export default Image;
