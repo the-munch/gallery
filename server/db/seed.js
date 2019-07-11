e873d130ec9c04a5e471f45f7e1fe960b93d56fd
Tina Le <tinale09@gmail.com>
2019-07-11 10:10:12 -0700

    database refill with smaller images

--- server/db/seed.js
+++ server/db/seed.js
@@ -3,15 +3,15 @@ var db = require('./index.js');
 
 //Function to generate Users without Images Array
 
-const seedUsers = () => {
-  let users = [];
-  let count = 0; 
-  while (count < 20){
-    users.push([faker.name.findName()]);  
-    count++; 
-  }
-  return users; 
-}; 
+// const seedUsers = () => {
+//   let users = [];
+//   let count = 0; 
+//   while (count < 20){
+//     users.push([faker.name.findName()]);  
+//     count++; 
+//   }
+//   return users; 
+// }; 
 
 //Function to generate Users with Images Array 
 
@@ -25,13 +25,24 @@ const seedURLUsers = () => {
 
 //Function to generate Images Information Array 
 
+// const seedImages = () => {
+//   let images = [];
+//   for (let i = 1; i < 101; i++){
+//     images.push([`https://munch-gallery.s3-us-west-1.amazonaws.com/munch${i}.jpg`, faker.lorem.words(), Math.ceil(Math.random() * 40)]);
+//   }
+//   return images; 
+// }; 
+
+
 const seedImages = () => {
   let images = [];
-  for (let i = 1; i < 101; i++){
-    images.push([`https://munch-gallery.s3-us-west-1.amazonaws.com/munch${i}.jpg`, faker.lorem.words(), Math.ceil(Math.random() * 40)]);
+  for (let i = 1; i < 21; i++){
+    images.push([`https://munch-gallery.s3-us-west-1.amazonaws.com/Small/munch${i}.jpg`, faker.lorem.words(), Math.ceil(Math.random() * 20)]);
   }
   return images; 
 }; 
+
+
 
 //Function to populate database with users w/o images
 
@@ -75,13 +86,22 @@ const seedAllImages = (callback) => {
 
 //Seed Database with all functions
 
-seedAllUsers(()=>seedAllURLUsers(()=>seedAllImages((err, message) => {
+// seedAllUsers(()=>seedAllURLUsers(()=>seedAllImages((err, message) => {
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log(message)
+//   }
+//   })));
+
+seedAllURLUsers(()=>seedAllImages((err, message) => {
   if (err){
     console.log(err);
   } else {
     console.log(message)
   }
-  })));
+  }));
+
 
 
 
